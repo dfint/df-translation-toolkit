8251e3bc40292d4bd37d54037eb06b19e1370294
Mike Manturov <insolor@users.noreply.github.com>
2015-01-08 23:14:09 +0300

    Fixed typo (form->from) in the function call

--- raw2pot.py
+++ raw2pot.py
@@ -2,5 +2,5 @@ import sys
 
 from dfgettext import *
 
-for context, item in ExtractTranslatablesFormRaws(sys.stdin):
+for context, item in ExtractTranslatablesFromRaws(sys.stdin):
     print(FormatPO(msgid=item,msgstr="",msgctxt=context))
