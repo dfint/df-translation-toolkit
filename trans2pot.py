9d2efc35c28fc3d9534f3f2291a73a68c0a7240a
Mike Manturov <insolor@gmail.com>
2015-02-03 13:43:55 +0300

    Improved LoadMO(). Close issue #1. Up to version 0.40.24

--- trans2pot.py
+++ trans2pot.py
@@ -1,6 +1,6 @@
 ﻿from dfgettext import *
 
-template = LoadStringDump('stringdump_0_40_12.txt')
+template = LoadStringDump('stringdump_0_40_24_noprefix.txt')
 ignorelist = {line[1] for line in LoadDSV('ignored.txt')}
 # dictionary = LoadFromTrans('trans.txt')
 SavePOT('DwarfFortress.pot',template,ignorelist)
