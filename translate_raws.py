d9ba1d4a7e03066429faf98f8549b22cb96a4fa0
Insolor <Insolor@localhost>
2015-02-08 18:23:12 +0300

    Patch raws in the given directory

--- translate_raws.py
+++ translate_raws.py
@@ -1,11 +1,27 @@
 import sys
+import os
+import shutil
 
 from dfgettext import *
 
-if len(sys.argv)<2:
+if len(sys.argv)<3:
     sys.exit()
 
-dictionary = {(item['msgid'],item['msgctxt']):item['msgstr'] for item in LoadMO(sys.argv[1])}
+mofilename = sys.argv[1]
+dictionary = {(item['msgid'],item['msgctxt']):item['msgstr'] for item in LoadMO(mofilename)}
 
-for line in translate_raw_file(sys.stdin, dictionary):
-    print(line)
+path = sys.argv[2]
+raws = os.listdir(path)
+for file_name in raws:
+    basename, ext = os.path.splitext(file_name)
+    if ext == '.txt':
+        bak_name = os.path.join(path, basename+'.bak')
+        raw_file = os.path.join(path, file_name)
+        
+        if not os.path.exists(bak_name):
+            shutil.copy(raw_file, bak_name)
+        
+        with open(bak_name) as src:
+            with open(raw_file, 'w', encoding='cp1251') as dest:
+                for line in translate_raw_file(src, dictionary):
+                    print(line, file=dest)
