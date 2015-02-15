36e3cbab64fc8036b88089b0c85375947099e4d9
Mike Manturov <insolor@users.noreply.github.com>
2015-02-16 02:05:02 +0300

    Added test_raws_mo.py

---
+++ test_raws_mo.py
@@ -0,0 +1,18 @@
+import sys
+
+from dfgettext import LoadMO
+
+from collections import defaultdict, Counter
+
+trans = defaultdict(Counter)
+
+if len(sys.argv)>1:
+    for item in LoadMO(sys.argv[1]):
+        if item['msgid']:
+            trans[item['msgid']][item['msgstr']]+=1
+
+    for key in sorted(trans, key=lambda x: -(len(trans[x])*1000+sum(trans[x].values()))):
+        print('msgid: ', key)
+        print('msgstrs: ', end='')
+        print(trans[key])
+        print()
