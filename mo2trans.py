166fe62e0c3af0f59e4f41f59b3d72ff0e1af341
Mike Manturov <insolor@users.noreply.github.com>
2015-02-21 00:32:27 +0300

    Unfinished: Pass a file object instead of filenames to the functions, raise exception in case of wrong mo-file given

--- mo2trans.py
+++ mo2trans.py
@@ -1,12 +1,15 @@
 import sys
+
 from dfgettext import *
 
-dictionary = {item['msgid']:item['msgstr'] for item in LoadMO('out.mo')}
-template = LoadStringDump('stringdump_0_40_24_noprefix.txt')
+with open(sys.argv[1], 'rb') as mofile:
+    dictionary = {item['msgid']:item['msgstr'] for item in LoadMO(mofile)}
 
-exclusions = {'Histories of '}
+with open(sys.argv[2]) as stringdump:
+    template = LoadStringDump(stringdump)
 
-with open('tmp_trans.txt','w',encoding='cp1251') as tmp_trans:
+    exclusions = {'Histories of '}
+
     for id, original_string in template:
         if original_string in dictionary:
             translation = dictionary[original_string]
@@ -14,8 +17,8 @@ with open('tmp_trans.txt','w',encoding='cp1251') as tmp_trans:
                 if original_string not in exclusions:
                     if original_string[0]==' ' and translation[0]!=' ' and translation[0]!=',':
                         translation = ' '+translation
-                        print("Warning: leading space added for the translation of the '%s' string." % original_string)
+                        print("Warning: leading space added for the translation of the '%s' string." % original_string, file = sys.stderr)
                     if original_string[-1]==' ' and translation[-1]!=' ':
                         translation = translation+' '
-                        print("Warning: trailing space added for the translation of the '%s' string." % original_string)
-                print("%s|%s|%s|" % (id, original_string, translation), file=tmp_trans)
+                        print("Warning: trailing space added for the translation of the '%s' string." % original_string, file = sys.stderr)
+                print("%s|%s|%s|" % (id, original_string, translation))
