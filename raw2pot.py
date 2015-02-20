635167dbd0ae4065fb6a3b48776f5d53ebee5b1c
Mike Manturov <insolor@users.noreply.github.com>
2015-02-21 01:47:21 +0300

    Implement raw2pot.py, make translate_raws.py ignore 'language_*.txt' files

--- raw2pot.py
+++ raw2pot.py
@@ -1,6 +1,19 @@
 import sys
+import os
 
 from dfgettext import *
 
-for context, item in ExtractTranslatablesFromRaws(sys.stdin):
-    print(FormatPO(msgid=item, msgstr="", msgctxt=context))
+if len(sys.argv)<2:
+    path = '.'
+else:
+    path = sys.argv[1]
+
+raws = filter(lambda x: not x.startswith('language_'), os.listdir(path))
+
+for file_name in raws:
+    fullpath = os.path.join(path, file_name)
+    if os.path.isfile(fullpath) and file_name.endswith('.txt'):
+        with open(fullpath) as file:
+            for context, item in ExtractTranslatablesFromRaws(file):
+                print('#:', file_name)
+                print(FormatPO(msgid=item, msgstr="", msgctxt=context))
