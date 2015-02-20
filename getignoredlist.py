80fb48fa6e3b3eb272091467474b875edc2a9a3e
Mike Manturov <insolor@users.noreply.github.com>
2015-02-21 00:39:58 +0300

    Update and improve getignoredlist.py

--- getignoredlist.py
+++ getignoredlist.py
@@ -1,13 +1,11 @@
 from dfgettext import *
 
-string_dump = LoadStringDump('stringdump_0_40_11.txt')
+with open('trans.txt') as transfile:
+    trans = LoadFromTrans(transfile)
 
-trans = LoadFromTrans('trans.txt')
-
-ignored = []
-for item in string_dump:
-    if item[1] not in trans:
-        ignored.append(item[1])
-    
-for item in sorted(ignored):
-    print('|%s|' % item)
+with open('stringdump_0_40_24_noprefix.txt') as dumpfile:
+    string_dump = LoadStringDump(dumpfile)
+    ignored = (item[1] for item in string_dump if item[1] not in trans)
+        
+    for item in sorted(ignored):
+        print('|%s|' % item)
