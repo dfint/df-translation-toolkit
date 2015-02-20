54470893de9e68094dc5787113065b99233be30b
Insolor <Insolor@localhost>
2015-02-20 23:55:54 +0300

    Don't put lines where translation equals to the original string to the trans.txt

--- mo2trans.py
+++ mo2trans.py
@@ -10,11 +10,12 @@ with open('tmp_trans.txt','w',encoding='cp1251') as tmp_trans:
     for id, original_string in template:
         if original_string in dictionary:
             translation = dictionary[original_string]
-            if original_string not in exclusions:
-                if original_string[0]==' ' and translation[0]!=' ' and translation[0]!=',':
-                    translation = ' '+translation
-                    print("Warning: leading space added for the translation of the '%s' string." % original_string)
-                if original_string[-1]==' ' and translation[-1]!=' ':
-                    translation = translation+' '
-                    print("Warning: trailing space added for the translation of the '%s' string." % original_string)
-            print("%s|%s|%s|" % (id, original_string, translation), file=tmp_trans)
+            if translation != original_string:
+                if original_string not in exclusions:
+                    if original_string[0]==' ' and translation[0]!=' ' and translation[0]!=',':
+                        translation = ' '+translation
+                        print("Warning: leading space added for the translation of the '%s' string." % original_string)
+                    if original_string[-1]==' ' and translation[-1]!=' ':
+                        translation = translation+' '
+                        print("Warning: trailing space added for the translation of the '%s' string." % original_string)
+                print("%s|%s|%s|" % (id, original_string, translation), file=tmp_trans)
