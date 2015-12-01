456f2f00ab5034e92d86af6c0cfbdc68f55ac154
Insolor <Insolor@localhost>
2015-12-02 01:49:51 +0300

    Fix codepage name in trans2pot.py from cp65001 to utf-8

--- trans2pot.py
+++ trans2pot.py
@@ -6,5 +6,5 @@ with open(sys.argv[1]) as stringdump:
     with open('ignored.txt') as ignored:
         ignorelist = {line[1] for line in load_dsv(ignored)}
     
-    with open('DwarfFortress.pot', 'w', encoding='cp65001') as potfile:
+    with open('DwarfFortress.pot', 'w', encoding='utf-8') as potfile:
         save_pot(potfile, template, ignorelist)
