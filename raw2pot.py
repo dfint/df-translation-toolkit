73fd3dd242e262cfbf7aad66c9575e64632a2986
Insolor <Insolor@localhost>
2015-12-02 01:49:13 +0300

    Write line number annotation to the pot file of raws

--- raw2pot.py
+++ raw2pot.py
@@ -13,9 +13,10 @@ raws = filter(lambda x: not x.startswith('language_'), os.listdir(path))
 potfilename = sys.argv[2]
 with open(potfilename, 'w', encoding='utf-8') as potfile:
     for file_name in raws:
+        print(file_name, file=sys.stderr)
         fullpath = os.path.join(path, file_name)
         if os.path.isfile(fullpath) and file_name.endswith('.txt'):
             with open(fullpath) as file:
-                for context, item in extract_translatables_from_raws(file):
-                    print('#:', file_name, file=potfile)
+                for context, item, line_number in extract_translatables_from_raws(file):
+                    print('#: %s:%d' % (file_name, line_number), file=potfile)
                     print(format_po(msgid=item, msgstr="", msgctxt=context), file=potfile)
