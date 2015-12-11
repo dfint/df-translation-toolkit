ee17bbb8fd47972f6e7b5dfa4a7f04effe2602a3
Mike Manturov <insolor@users.noreply.github.com>
2015-12-12 01:10:30 +0300

    Get ignorelist filename form args in trans2pot.py

--- trans2pot.py
+++ trans2pot.py
@@ -3,8 +3,11 @@ from dfgettext import *
 
 with open(sys.argv[1]) as stringdump:
     template = load_string_dump(stringdump)
-    with open('ignored.txt') as ignored:
-        ignorelist = {line[1] for line in load_dsv(ignored)}
+    if len(sys.argv) > 1:
+        with open(sys.argv[2]) as ignored:
+            ignorelist = {line[1] for line in load_dsv(ignored)}
+    else:
+        ignorelist = set()
     
     with open('DwarfFortress.pot', 'w', encoding='utf-8') as potfile:
         save_pot(potfile, template, ignorelist)
