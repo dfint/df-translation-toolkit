f8bac45fd760e1054d9ca160fb9804ae53673066
Insolor <Insolor@localhost>
2016-02-03 00:18:28 +0300

    translate_plain_text.py fixed

--- translate_plain_text.py
+++ translate_plain_text.py
@@ -3,7 +3,8 @@
 import os
 import sys
 import dfgettext
-from dfgettext import parse_plain_text_file
+import shutil
+from dfgettext import parse_plain_text_file, load_po
 
 pofilename = sys.argv[1]
 with open(pofilename, 'r', encoding='utf-8') as pofile:
@@ -14,25 +15,31 @@ if len(sys.argv) > 2:
 else:
     path = '.'
 
-files = os.listdir(path)
-
 join_paragraphs = '--split' not in sys.argv[1:]
 
-for file_name in files:
-    basename, ext = os.path.splitext(file_name)
-    if ext == '.txt':
-        bak_name = os.path.join(path, basename+'.bak')
-        dest_name = os.path.join(path, file_name)
-        
-        if not os.path.exists(bak_name):
-            shutil.copy(dest_name, bak_name)
-        
-        with open(bak_name) as src:
-            with open(dest_name, 'w', encoding='cp1251') as dest:
-                print(dest_name, file=sys.stderr)
-                for text_block, is_translatable in parse_plain_text_file(src):
-                    text_block = text_block.rstrip('\n')
-                    if is_translatable:
-                        print(dictionary.get(text_block, text_block), file=dest)
-                    else:
-                        print(text_block, file=dest)
+for cur_dir, _, files in os.walk(path):
+    for file_name in files:
+        basename, ext = os.path.splitext(file_name)
+        if ext == '.txt':
+            bak_name = os.path.join(cur_dir, basename+'.bak')
+            dest_name = os.path.join(cur_dir, file_name)
+            
+            if not os.path.exists(bak_name):
+                shutil.copy(dest_name, bak_name)
+            
+            with open(bak_name) as src:
+                with open(dest_name, 'w', encoding='cp1251') as dest:
+                    print(dest_name, file=sys.stderr)
+                    for text_block, is_translatable in parse_plain_text_file(src):
+                        # print('>',repr(text_block), is_translatable)
+                        text_block = text_block.rstrip('\n')
+                        if is_translatable:
+                            if text_block in dictionary:
+                                translation = dictionary[text_block]
+                                if not translation:
+                                    translation = text_block
+                            else:
+                                translation = text_block
+                            print(translation, file=dest)
+                        else:
+                            print(text_block, file=dest)
