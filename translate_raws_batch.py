be34ccfb1259ccc947eab6b3f77c11d068721103
Mike Manturov <insolor@users.noreply.github.com>
2016-02-04 02:50:47 +0300

    Improve and simplify translate_raws_batch.py

--- translate_raws_batch.py
+++ translate_raws_batch.py
@@ -5,12 +5,9 @@ from translate_raws import translate_raws
 po_file_name = r'..\python-transifex\ru-raw-objects.po'
 base_path = r"d:\Games\PeridexisErrant's Starter Pack 0.42.05-r01"
 
-tilesets_path = os.path.join(base_path, 'LNP\graphics')
-dirs = [os.path.join(tilesets_path, x, r'raw\objects') for x in list(os.listdir(tilesets_path))]
-dirs += [os.path.join(base_path, 'Dwarf Fortress 0.42.05', r'raw\objects')]
-
-for dir in dirs:
-    print(dir+':', file=sys.stderr)
-    print('', file=sys.stderr)
-    translate_raws(po_file_name, dir, 'cp1251')
-    print('', file=sys.stderr)
+for cur_dir, _, files in os.walk(base_path):
+    if cur_dir.endswith(r'raw\objects'):
+        print(cur_dir, file=sys.stderr)
+        print(file=sys.stderr)
+        translate_raws(po_file_name, cur_dir, 'cp1251')
+        print(file=sys.stderr)
