51c3030f895819b4b5273c72a0c192d49e1464c9
Mike Manturov <insolor@users.noreply.github.com>
2016-02-14 21:53:28 +0300

    Improve translate_raws_batch.py

--- translate_plain_text.py
+++ translate_plain_text.py
@@ -6,38 +6,43 @@ import dfgettext
 import shutil
 from dfgettext import parse_plain_text_file, load_po
 
-pofilename = sys.argv[1]
-with open(pofilename, 'r', encoding='utf-8') as pofile:
-    dictionary = {item['msgid']: item['msgstr'] for item in load_po(pofile)}
 
-if len(sys.argv) > 2:
-    path = sys.argv[2]
-else:
-    path = '.'
-
-join_paragraphs = '--split' not in sys.argv[1:]
-
-for cur_dir, _, files in os.walk(path):
-    for file_name in files:
-        basename, ext = os.path.splitext(file_name)
-        if ext == '.txt':
-            bak_name = os.path.join(cur_dir, basename+'.bak')
-            dest_name = os.path.join(cur_dir, file_name)
-            
-            if not os.path.exists(bak_name):
-                shutil.copy(dest_name, bak_name)
-            
-            with open(bak_name) as src:
-                with open(dest_name, 'w', encoding='cp1251') as dest:
-                    print(dest_name, file=sys.stderr)
-                    for text_block, is_translatable in parse_plain_text_file(src, join_paragraphs):
-                        # print('>', repr(text_block))
-                        text_block = text_block.rstrip('\n')
-                        if text_block in dictionary:
-                            translation = dictionary[text_block]
-                            if not translation:
+def translate_plain_text(pofilename, path, encoding, join_paragraphs=True):
+    with open(pofilename, 'r', encoding='utf-8') as pofile:
+        dictionary = {item['msgid']: item['msgstr'] for item in load_po(pofile)}
+        
+    for cur_dir, _, files in os.walk(path):
+        for file_name in files:
+            basename, ext = os.path.splitext(file_name)
+            if ext == '.txt':
+                bak_name = os.path.join(cur_dir, basename+'.bak')
+                dest_name = os.path.join(cur_dir, file_name)
+                
+                if not os.path.exists(bak_name):
+                    shutil.copy(dest_name, bak_name)
+                
+                with open(bak_name) as src:
+                    with open(dest_name, 'w', encoding=encoding) as dest:
+                        print(dest_name, file=sys.stderr)
+                        for text_block, is_translatable in parse_plain_text_file(src, join_paragraphs):
+                            text_block = text_block.rstrip('\n')
+                            if text_block in dictionary:
+                                translation = dictionary[text_block]
+                                if not translation:
+                                    translation = text_block
+                            else:
                                 translation = text_block
-                        else:
-                            translation = text_block
-                        # print('<', repr(translation))
-                        print(translation, file=dest)
+                            print(translation, file=dest)
+
+
+if __name__ == "__main__":
+    pofilename = sys.argv[1]
+    
+    if len(sys.argv) > 2:
+        path = sys.argv[2]
+    else:
+        path = '.'
+
+    join_paragraphs = '--split' not in sys.argv[1:]
+    
+    translate_plain_text(pofilename, path, 'cp1251', join_paragraphs)
--- translate_raws_batch.py
+++ translate_raws_batch.py
@@ -1,13 +1,39 @@
 import os
 import sys
 from translate_raws import translate_raws
+from translate_plain_text import translate_plain_text
 
 base_path = sys.argv[1]
-po_file_name = sys.argv[2]
+po_file_path = sys.argv[2]
+encoding = 'cp1251' if len(sys.argv) < 4 else sys.argv[3]
+
+lang_prefix = 'ru'
+
+patterns = {
+    r'raw\objects': dict(
+        po_filename=lang_prefix+'-raw-objects.po',
+        func=translate_raws,
+    ),
+    r'data_src': dict(
+        po_filename=lang_prefix+'-uncompressed.po',
+        func=lambda *args: translate_plain_text(*args, join_paragraphs=True),
+    ),
+    r'data\speech': dict(
+        po_filename=lang_prefix+'-speech.po',
+        func=lambda *args: translate_plain_text(*args, join_paragraphs=False),
+    ),
+    r'raw\objects\text': dict(
+        po_filename=lang_prefix+'-text.po',
+        func=lambda *args: translate_plain_text(*args, join_paragraphs=False),
+    ),
+}
 
 for cur_dir, _, files in os.walk(base_path):
-    if cur_dir.endswith(r'raw\objects'):
-        print(cur_dir, file=sys.stderr)
-        print(file=sys.stderr)
-        translate_raws(po_file_name, cur_dir, 'cp1251')
-        print(file=sys.stderr)
+    for pattern in patterns:
+        if cur_dir.endswith(pattern):
+            print(cur_dir, file=sys.stderr)
+            print(file=sys.stderr)
+            po_filename =  os.path.join(po_file_path, patterns[pattern]['po_filename'])
+            func = patterns[pattern]['func']
+            func(po_filename), cur_dir, encoding)
+            print(file=sys.stderr)
