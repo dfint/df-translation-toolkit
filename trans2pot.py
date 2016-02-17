2c46d598f37f09c52ce0b5bb9b5e29a5afa5dfa0
Mike Manturov <insolor@users.noreply.github.com>
2016-02-17 14:45:27 +0300

    Fix IndexError in trans2pot when ignore list is not provided

--- trans2pot.py
+++ trans2pot.py
@@ -3,7 +3,7 @@ from dfgettext import *
 
 with open(sys.argv[1]) as stringdump:
     template = (line.rstrip('\n') for line in stringdump)
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         with open(sys.argv[2]) as ignored:
             ignorelist = {line.rstrip('\n').strip('|') for line in ignored}
     else:
