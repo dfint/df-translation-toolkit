366288e19eafad31dcc44ff9fef5bb14bcf45627
Mike Manturov <insolor@users.noreply.github.com>
2016-03-16 22:34:35 +0300

    Replaced MO with PO in po2csv.py description

--- po2csv.py
+++ po2csv.py
@@ -7,7 +7,7 @@ from collections import OrderedDict
 from dfgettext import *
 
 print(sys.argv, file=sys.stderr)
-parser = argparse.ArgumentParser(add_help=True, description='A convertor from MO gettext format to a delimiter-separated values file')
+parser = argparse.ArgumentParser(add_help=True, description='A convertor from PO gettext format to a delimiter-separated values file')
 parser.add_argument('inputfile', help='Source PO file name')
 parser.add_argument('outputfile', help='A name of the output file')
 parser.add_argument('codepage', help='Encoding of the outfile (cp427, cp850, cp860, cp1251 etc.)')
