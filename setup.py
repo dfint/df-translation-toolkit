d588961f287e900f8ca3d2291a11fd3dad0167a3
insolor <insolor@localhost>
2016-08-30 14:33:49 +0300

    Add basic __init__.py and setup.py files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='df_gettext_toolkit',
+      version='0.1',
+      # description='',
+      url='http://bitbucket.org/dfint/df-gettext-toolkit',
+      author='insolor',
+      author_email='insolor@gmail.com',
+      # license='MIT',
+      packages=[],
+      zip_safe=False)
