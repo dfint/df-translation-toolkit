f570538bc94b357e592e8948b3e626aedfd013df
insolor <insolor@localhost>
2017-04-23 23:39:34 +0300

    Add prefix once

--- df_gettext_toolkit/translate_raws_batch.py
+++ df_gettext_toolkit/translate_raws_batch.py
@@ -11,19 +11,19 @@ prefix = '' if len(sys.argv) < 5 else sys.argv[4]
 
 patterns = {
     r'raw\objects': dict(
-        po_filename=prefix+'raw-objects.po',
+        po_filename='raw-objects.po',
         func=translate_raws,
     ),
     r'data_src': dict(
-        po_filename=prefix+'uncompressed.po',
+        po_filename='uncompressed.po',
         func=lambda *args: translate_plain_text(*args, join_paragraphs=True),
     ),
     r'data\speech': dict(
-        po_filename=prefix+'speech.po',
+        po_filename='speech.po',
         func=lambda *args: translate_plain_text(*args, join_paragraphs=False),
     ),
     r'raw\objects\text': dict(
-        po_filename=prefix+'text.po',
+        po_filename='text.po',
         func=lambda *args: translate_plain_text(*args, join_paragraphs=False),
     ),
 }
@@ -33,7 +33,7 @@ for cur_dir, _, files in os.walk(base_path):
         if cur_dir.endswith(pattern):
             print(cur_dir, file=sys.stderr)
             print(file=sys.stderr)
-            po_filename =  os.path.join(po_file_path, patterns[pattern]['po_filename'])
+            po_filename =  os.path.join(po_file_path, prefix+patterns[pattern]['po_filename'])
             func = patterns[pattern]['func']
             func(po_filename, cur_dir, encoding)
             print(file=sys.stderr)
