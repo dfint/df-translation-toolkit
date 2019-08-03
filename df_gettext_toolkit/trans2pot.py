3ff4ef5779b081448d5deb4839624ad47914f553
Mike Manturov <insolor@users.noreply.github.com>
2019-08-03 22:54:28 +0300

    Use ignore rules in trans2pot.py

--- df_gettext_toolkit/trans2pot.py
+++ df_gettext_toolkit/trans2pot.py
@@ -1,13 +1,11 @@
 ﻿import sys
 
 from .po import save_pot
+from .df_ignore_string_rules import ignore_all
 
 with open(sys.argv[1]) as stringdump:
     template = (line.rstrip('\n') for line in stringdump)
-    if len(sys.argv) > 2:
-        with open(sys.argv[2]) as ignored:
-            ignorelist = {line.rstrip('\n').strip('|') for line in ignored}
-    else:
-        ignorelist = set()
+    ignore_rules = ignore_all if '--no-ignore' not in sys.argv else lambda s: False
+
     with open('DwarfFortress.pot', 'w', encoding='utf-8') as potfile:
-        save_pot(potfile, (line for line in template if line not in ignorelist))
+        save_pot(potfile, (line for line in template if not ignore_rules(line)))
