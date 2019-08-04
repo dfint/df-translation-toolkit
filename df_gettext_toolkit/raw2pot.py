fd83a48d81fcb841d4fbedf902f6966c8c7fb004
Mike Manturov <insolor@users.noreply.github.com>
2019-08-04 19:42:12 +0300

    Sort raw files by name, print only names of processed files

--- df_gettext_toolkit/raw2pot.py
+++ df_gettext_toolkit/raw2pot.py
@@ -14,10 +14,10 @@ def main(pot_filename, path, source_encoding):
     raw_files = filter(lambda x: not x.startswith('language_'), os.listdir(path))
 
     with open(pot_filename, 'w', encoding='utf-8') as potfile:
-        for file_name in raw_files:
-            print(file_name, file=sys.stderr)
+        for file_name in sorted(raw_files):
             full_path = os.path.join(path, file_name)
             if os.path.isfile(full_path) and file_name.endswith('.txt'):
+                print(file_name, file=sys.stderr)
                 with open(full_path, encoding=source_encoding) as file:
                     for context, item, line_number in extract_translatables_from_raws(file):
                         print('#: %s:%d' % (file_name, line_number), file=potfile)  # source file : line number
