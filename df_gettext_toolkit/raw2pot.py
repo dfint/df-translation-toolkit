d4f148c7f430dba65c3680982e80f295309d97d4
insolor <insolor@localhost>
2019-08-04 19:40:13 +0300

    Open raw files in cp437 encoding by default

--- df_gettext_toolkit/raw2pot.py
+++ df_gettext_toolkit/raw2pot.py
@@ -9,7 +9,8 @@ from .po import format_po
 @click.command()
 @click.argument('pot_filename')
 @click.argument('path', default='.')
-def main(pot_filename, path):
+@click.argument('source_encoding', default='cp437')
+def main(pot_filename, path, source_encoding):
     raw_files = filter(lambda x: not x.startswith('language_'), os.listdir(path))
 
     with open(pot_filename, 'w', encoding='utf-8') as potfile:
@@ -17,7 +18,7 @@ def main(pot_filename, path):
             print(file_name, file=sys.stderr)
             full_path = os.path.join(path, file_name)
             if os.path.isfile(full_path) and file_name.endswith('.txt'):
-                with open(full_path) as file:
+                with open(full_path, encoding=source_encoding) as file:
                     for context, item, line_number in extract_translatables_from_raws(file):
                         print('#: %s:%d' % (file_name, line_number), file=potfile)  # source file : line number
                         print(format_po(msgid=item, msgstr="", msgctxt=context), file=potfile)
