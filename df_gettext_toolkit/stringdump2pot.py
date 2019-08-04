d4729bf6500bfb7c6c82052641cddc804c39c827
insolor <insolor@localhost>
2019-08-04 14:26:20 +0300

    Add click support

--- df_gettext_toolkit/stringdump2pot.py
+++ df_gettext_toolkit/stringdump2pot.py
@@ -1,11 +1,21 @@
-﻿import sys
+﻿import click
 
 from .po import save_pot
 from .df_ignore_string_rules import ignore_all
 
-with open(sys.argv[1]) as stringdump:
-    template = (line.rstrip('\n') for line in stringdump)
-    ignore_rules = ignore_all if '--no-ignore' not in sys.argv else lambda s: False
 
-    with open('DwarfFortress.pot', 'w', encoding='utf-8') as potfile:
-        save_pot(potfile, (line for line in template if not ignore_rules(line)))
+@click.command()
+@click.argument('source_file')
+@click.argument('destination_file')
+@click.option('--no-ignore', default=False)
+def main(source_file, destination_file, no_ignore=False):
+    with open(source_file) as stringdump:
+        template = (line.rstrip('\n') for line in stringdump)
+        ignore_rules = (lambda s: False) if no_ignore else ignore_all
+        filtered_lines = (line for line in template if not ignore_rules(line))
+        with open(destination_file, 'w', encoding='utf-8') as potfile:
+            save_pot(potfile, filtered_lines)
+
+
+if __name__ == '__main__':
+    main()
