b8afee1661674c2d1e5ce97c175fc65971cec7fd
insolor <insolor@gmail.com>
2020-07-12 23:46:48 +0300

    Revive po2csv module

---
+++ df_gettext_toolkit/po2csv.py
@@ -0,0 +1,45 @@
+#! python3
+import sys
+import argparse
+import csv
+from collections import OrderedDict
+
+from .po import load_po, escape_string
+from .cleanup_string import cleanup
+
+print(sys.argv, file=sys.stderr)
+parser = argparse.ArgumentParser(add_help=True, description='A convertor from PO gettext format to a delimiter-separated values file')
+parser.add_argument('inputfile', help='Source PO file name')
+parser.add_argument('outputfile', help='A name of the output file')
+parser.add_argument('codepage', help='Encoding of the outfile (cp427, cp850, cp860, cp1251 etc.)', default='utf8')
+
+args = parser.parse_args(sys.argv[1:])
+
+with open(args.inputfile, 'r', encoding='utf8') as pofile:
+    dictionary = OrderedDict((item['msgid'], item['msgstr']) for item in load_po(pofile) if item['msgid'])
+
+exclusions_left = {'  Choose Name  ', '  Trade Agreement with '}
+exclusions_right = {'  Choose Name  '}
+
+with open(args.outputfile, 'w', newline='', encoding=args.codepage, errors='replace') as outfile:
+    writer = csv.writer(outfile, dialect='unix')
+    if args.codepage == 'cp1251':
+        exclusions_right.add('Histories of ')
+    
+    for original_string in dictionary:
+        translation = dictionary[original_string]
+        if original_string and translation and translation != original_string:
+            if (original_string not in exclusions_left and
+                    original_string[0]==' ' and
+                        translation[0]!=' ' and
+                        translation[0]!=','):
+                translation = ' ' + translation
+                print("Leading space added to the translation of the string: %r" % original_string, file=sys.stderr)
+            
+            if original_string not in exclusions_right and original_string[-1]==' ' and translation[-1]!=' ':
+                translation += ' '
+                print("Trailing space added to the translation of the string: %r" % original_string, file=sys.stderr)
+            
+            translation = cleanup(translation)
+            
+            writer.writerow([escape_string(original_string), escape_string(translation)])
