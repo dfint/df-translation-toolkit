a18df0effffe11108027fb55fee12cf6e5a499b3
insolor <insolor@gmail.com>
2020-08-09 22:43:30 +0300

    Using pathlib instead of os.path

--- df_gettext_toolkit/translate_raws_batch.py
+++ df_gettext_toolkit/translate_raws_batch.py
@@ -1,5 +1,6 @@
-import os
 import sys
+
+from pathlib import Path
 
 import click
 
@@ -14,33 +15,34 @@ from .translate_plain_text import translate_plain_text
 @click.option('--prefix', default='')
 def main(base_path, po_file_path, encoding, prefix):
     patterns = {
-        r'raw\objects': dict(
+        'raw/objects': dict(
             po_filename='raw-objects.po',
             func=translate_raws,
         ),
-        r'data_src': dict(
+        'data_src': dict(
             po_filename='uncompressed.po',
             func=lambda *args: translate_plain_text(*args, join_paragraphs=True),
         ),
-        r'data\speech': dict(
+        'data/speech': dict(
             po_filename='speech.po',
             func=lambda *args: translate_plain_text(*args, join_paragraphs=False),
         ),
-        r'raw\objects\text': dict(
+        'raw/objects/text': dict(
             po_filename='text.po',
             func=lambda *args: translate_plain_text(*args, join_paragraphs=False),
         ),
     }
 
-    for cur_dir, _, files in os.walk(base_path):
-        print(cur_dir)
-        for pattern in patterns:
-            if cur_dir.replace('/', '\\').endswith(pattern):
-                print(f"Matched {pattern} pattern")
-                print(cur_dir, file=sys.stderr)
-                print(file=sys.stderr)
-                po_filename = os.path.join(po_file_path, prefix+patterns[pattern]['po_filename'])
-                func = patterns[pattern]['func']
-                for filename in func(po_filename, cur_dir, encoding):
-                    print(filename, file=sys.stderr)
-                print(file=sys.stderr)
+    for cur_dir in Path(base_path).glob("*"):
+        if cur_dir.is_dir():
+            print(cur_dir)
+            for pattern in patterns:
+                if cur_dir.match('*/' + pattern):
+                    print(f"Matched {pattern} pattern")
+                    print(cur_dir, file=sys.stderr)
+                    print(file=sys.stderr)
+                    po_filename = Path.joinpath(po_file_path, prefix+patterns[pattern]['po_filename'])
+                    func = patterns[pattern]['func']
+                    for filename in func(po_filename, cur_dir, encoding):
+                        print(filename, file=sys.stderr)
+                    print(file=sys.stderr)
