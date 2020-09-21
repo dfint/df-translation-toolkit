5270b2b038c374a91137a53ebac0be7bb3ab4692
insolor <insolor@gmail.com>
2020-09-21 09:46:04 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
-df_raw_decoder = 'https://github.com/dfint/df_raw_decoder/archive/master.zip#egg=df-raw-decoder'
+df_raw_decoder = 'df-raw-decoder @ https://github.com/dfint/df_raw_decoder/archive/master.zip'
 
 setup(name='df_gettext_toolkit',
       version='0.1',
       # description='',
-      url='http://bitbucket.org/dfint/df-gettext-toolkit',
+      url='https://github.com/dfint/df-gettext-toolkit',
       author='insolor',
       author_email='insolor@gmail.com',
-      # license='MIT',
+      license='MIT',
       packages=find_packages(),
       zip_safe=False,
       install_requires=['click', df_raw_decoder])
