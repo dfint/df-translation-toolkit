6cfcc2d33f698b0b6c46be808fa32f246c6ebfaa
insolor <insolor@gmail.com>
2021-05-29 12:52:04 +0300

    Add flake8 config, refactoring of tests

--- tests/test_df_ignore_string_rules.py
+++ tests/test_df_ignore_string_rules.py
@@ -1,82 +1,120 @@
-from df_gettext_toolkit.df_ignore_string_rules import *
+import pytest
+
+import df_gettext_toolkit.df_ignore_string_rules as rules
 
 
-def test_ignore_xml():
-    assert ignore_xml("""<?xml version="1.0" encoding='CP437'?>""") is True
-    assert ignore_xml("No love   <->    Loves easily") is False
-    assert ignore_xml("<local_id>") is True
-    assert ignore_xml("</local_id>") is True
-    assert ignore_xml("<type>mead hall</type>") is True
-    assert ignore_xml("<no uniforms>") is False
+@pytest.mark.parametrize(
+    "string,ignore",
+    [
+        ("""<?xml version="1.0" encoding='CP437'?>""", True),
+        ("No love   <->    Loves easily", False),
+        ("<local_id>", True),
+        ("/<local_id>", True),
+        ("<type>mead hall</type>", True),
+        ("<no uniforms>", False),
+    ],
+)
+def test_ignore_xml(string, ignore):
+    assert rules.ignore_xml(string) is ignore
 
 
-def test_ignore_square_brackets():
-    assert ignore_square_brackets('We have many drinks to choose from.  [You receive a list.]') is False
-    assert ignore_square_brackets('[B]') is True
-    assert ignore_square_brackets('[PRO_SUB]') is True
-    assert ignore_square_brackets('  [You receive a detailed description.]') is False
-    assert ignore_square_brackets('[CREATURE:') is True
-    assert ignore_square_brackets('[C:4:0:1]') is True
-    assert ignore_square_brackets(' [C:7:0:1]and is [C:3:0:0]oblivious to reality') is False
-    assert ignore_square_brackets(' [DONE]') is False
-    assert ignore_square_brackets(' [MORE]') is False
-    assert ignore_square_brackets(" [CAN'T WORK]") is False
-    assert ignore_square_brackets(" [WITH YOU]") is False
-    assert ignore_square_brackets("([LIP])") is False
-    assert ignore_square_brackets(r"\t\t[SYN_AFFECTED_CLASS:GENERAL_POISON]") is True
-    assert ignore_square_brackets(r"\t\t[SYN_INJECTED][SYN_CONTACT][SYN_INHALED][SYN_INGESTED]") is True
-    assert ignore_square_brackets("[STATE_ADJ:ALL_SOLID:frozen ") is False
-    assert ignore_square_brackets("[STATE_ADJ:ALL_SOLID:") is True
-    assert ignore_square_brackets(":BP:BY_CATEGORY:ALL:EYE") is True
+@pytest.mark.parametrize(
+    "string,ignore",
+    [
+        ("We have many drinks to choose from.  [You receive a list.]", False),
+        ("[B]", True),
+        ("[PRO_SUB]", True),
+        ("  [You receive a detailed description.]", False),
+        ("[CREATURE:", True),
+        ("[C:4:0:1]", True),
+        (" [C:7:0:1]and is [C:3:0:0]oblivious to reality", False),
+        (" [DONE]", False),
+        (" [MORE]", False),
+        (" [WITH YOU]", False),
+        ("([LIP])", False),
+        (r"\t\t[SYN_AFFECTED_CLASS:GENERAL_POISON]", True),
+        (r"\t\t[SYN_INJECTED][SYN_CONTACT][SYN_INHALED][SYN_INGESTED]", True),
+        ("[STATE_ADJ:ALL_SOLID:frozen ", False),
+        ("[STATE_ADJ:ALL_SOLID:", True),
+        (":BP:BY_CATEGORY:ALL:EYE", True),
+    ],
+)
+def test_ignore_square_brackets(string, ignore):
+    assert rules.ignore_square_brackets(string) is ignore
 
 
-def test_ignore_paths():
-    assert ignore_paths('/stdout.txt') is True
-    assert ignore_paths('data/save/current') is True
-    assert ignore_paths('data/init/interface.txt') is True
-    assert ignore_paths('objects/b_detail_plan_*.txt') is True
-    assert ignore_paths('/Resting') is False
-    assert ignore_paths('Upright Spear/Spike') is False
-    assert ignore_paths('Track/Ramp (NW)') is False
-    assert ignore_paths('Unknown Body Group/Relation Token(s): ') is False
-    assert ignore_paths('data/save/*.*') is True
-    assert ignore_paths('.bmp') is True
-    assert ignore_paths('grinding.') is False
+@pytest.mark.parametrize(
+    "string,ignore",
+    [
+        ("/stdout.txt", True),
+        ("data/save/current", True),
+        ("data/init/interface.txt", True),
+        ("objects/b_detail_plan_*.txt", True),
+        ("/Resting", False),
+        ("Upright Spear/Spike", False),
+        ("Track/Ramp (NW)", False),
+        ("Unknown Body Group/Relation Token(s): ", False),
+        ("data/save/*.*", True),
+        (".bmp", True),
+        ("grinding.", False),
+    ],
+)
+def test_ignore_paths(string, ignore):
+    assert rules.ignore_paths(string) is ignore
 
 
 def test_ignore_filenames():
-    assert ignore_filenames("-detailed.bmp") is True
+    assert rules.ignore_filenames("-detailed.bmp") is True
 
 
 def test_ignore_underline_separated_words():
-    assert ignore_underline_separated_words("index1_11") is True
+    assert rules.ignore_underline_separated_words("index1_11") is True
 
 
 def test_ignore_dash_prepended_strings():
-    assert ignore_dash_prepended_strings('-world_sites_and_pops') is True
-    assert ignore_dash_prepended_strings('-site_map-') is True
+    assert rules.ignore_dash_prepended_strings("-world_sites_and_pops") is True
+    assert rules.ignore_dash_prepended_strings("-site_map-") is True
 
 
 def test_ignore_by_blacklisted_words():
-    assert ignore_by_blacklisted_words("*** Error(s) finalizing the creature ") is True
+    assert (
+        rules.ignore_by_blacklisted_words("*** Error(s) finalizing the creature ")
+        is True
+    )
 
 
-def test_ignore_camel_case():
-    assert ignore_camel_case("InitializeConditionVariable") is True
-    assert ignore_camel_case("Initialize") is False
-    assert ignore_camel_case("SleepConditionVariableCS") is True
-    assert ignore_camel_case("CAPS") is False
-    assert ignore_camel_case("RefusedID/") is True
-    assert ignore_camel_case("plF") is True
+@pytest.mark.parametrize(
+    "string,ignore",
+    [
+        ("InitializeConditionVariable", True),
+        ("Initialize", False),
+        ("SleepConditionVariableCS", True),
+        ("CAPS", False),
+        ("RefusedID/", True),
+        ("plF", True),
+    ],
+)
+def test_ignore_camel_case(string, ignore):
+    assert rules.ignore_camel_case(string) is ignore
 
 
 def test_ignore_by_blacklisted_substrings():
-    assert ignore_by_blacklisted_substrings('plotter (assassinate) placed out of bounds') is True
-    assert ignore_by_blacklisted_substrings('undefined local creature material set to default: ') is True
+    assert (
+        rules.ignore_by_blacklisted_substrings(
+            "plotter (assassinate) placed out of bounds"
+        )
+        is True
+    )
+    assert (
+        rules.ignore_by_blacklisted_substrings(
+            "undefined local creature material set to default: "
+        )
+        is True
+    )
 
 
 def test_all_ignore_rules():
-    assert all_ignore_rules('any text') is False
-    assert all_ignore_rules('Any text') is False
-    assert all_ignore_rules('Any text.') is False
-    assert all_ignore_rules('Another string: just a tests') is False
+    assert rules.all_ignore_rules("any text") is False
+    assert rules.all_ignore_rules("Any text") is False
+    assert rules.all_ignore_rules("Any text.") is False
+    assert rules.all_ignore_rules("Another string: just a tests") is False
--- tests/test_fix_translated_strings.py
+++ tests/test_fix_translated_strings.py
@@ -1,19 +1,26 @@
 import pytest
 
 from df_gettext_toolkit.fix_translated_strings import cleanup_string
-from df_gettext_toolkit.fix_translated_strings import fix_leading_spaces, fix_trailing_spaces, fix_spaces
+from df_gettext_toolkit.fix_translated_strings import (
+    fix_leading_spaces,
+    fix_trailing_spaces,
+    fix_spaces,
+)
 
 
 @pytest.mark.parametrize(
     "original,fixed",
     [
-        ('kožušček', 'kožušček'),  # must not remove diacritics
-        ('\ufeff\u200b', ''),  # remove zero width characters
-        ('—', '-'),  # use standard dash instead of Em Dash
+        ("kožušček", "kožušček"),  # must not remove diacritics
+        ("\ufeff\u200b", ""),  # remove zero width characters
+        ("—", "-"),  # use standard dash instead of Em Dash
         (""" ‘’”“ """, """ ''"" """),  # Fix quotes
-        ('¿Qué dirás?', '¿Qué dirás?'),  # Don't change Spanish question mark
-        ('¡Hace un frio helador!', '¡Hace un frio helador!'),  # Don't change Spanish exclamation mark
-    ]
+        ("¿Qué dirás?", "¿Qué dirás?"),  # Don't change Spanish question mark
+        (
+            "¡Hace un frio helador!",
+            "¡Hace un frio helador!",
+        ),  # Don't change Spanish exclamation mark
+    ],
 )
 def test_cleanup_string(original, fixed):
     assert cleanup_string(original) == fixed
@@ -26,7 +33,7 @@ def test_cleanup_string(original, fixed):
         ("test", " test", "test"),
         (" test", ", test", ", test"),
         (".  What do you command?", " . ¿Qué ordenas?", ". ¿Qué ordenas?"),
-    ]
+    ],
 )
 def test_fix_leading_spaces(text, translation, fixed):
     assert fix_leading_spaces(text, translation, set()) == fixed
@@ -37,7 +44,7 @@ def test_fix_leading_spaces(text, translation, fixed):
     [
         ("test ", "test", "test "),
         # ("test", "test ", "test"),
-    ]
+    ],
 )
 def test_fix_trailing_spaces(text, translation, fixed):
     assert fix_trailing_spaces(text, translation, set()) == fixed
@@ -47,7 +54,7 @@ def test_fix_trailing_spaces(text, translation, fixed):
     "text,translation,fixed",
     [
         (" embraces ", "obdivuje", " obdivuje "),
-    ]
+    ],
 )
 def test_fix_spaces(text, translation, fixed):
     assert fix_spaces(text, translation) == fixed
