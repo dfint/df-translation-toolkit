f29ef4ea10e0172fbea8b4aa7caea86c223668c0
insolor <insolor@gmail.com>
2021-06-02 15:19:52 +0300

    Use pathlib instead of os.path in create_pot_from_plain_text.py

--- df_gettext_toolkit/create_pot_from_plain_text.py
+++ df_gettext_toolkit/create_pot_from_plain_text.py
@@ -1,6 +1,5 @@
-import os
 import sys
-from operator import itemgetter
+from pathlib import Path
 
 import typer
 
@@ -12,20 +11,17 @@ def main(path: str = '.', split: bool = False):
     join_paragraphs = not split
 
     keys = set()
-    for cur_dir, _, files in sorted(os.walk(path), key=itemgetter(0)):
-        for file_name in sorted(files):
-            full_path = os.path.join(cur_dir, file_name)
-            if os.path.isfile(full_path) and os.path.splitext(file_name)[1] == '.txt':
-                print(full_path, file=sys.stderr)
-                with open(full_path) as file:
-                    for text_block, is_translatable, line_number in parse_plain_text_file(file, join_paragraphs):
-                        if not is_translatable:
-                            pass
-                        elif text_block in keys:
+    for file_path in sorted(Path(path).rglob("*.txt")):
+        if file_path.is_file():
+            print(file_path, file=sys.stderr)
+            with open(file_path) as file:
+                for text_block, is_translatable, line_number in parse_plain_text_file(file, join_paragraphs):
+                    if is_translatable:
+                        if text_block in keys:
                             print('Key already exists:', repr(text_block), file=sys.stderr)
                         else:
                             keys.add(text_block)
-                            print('#: %s:%d' % (file_name, line_number))
+                            print('#: %s:%d' % (file_path.name, line_number))
                             print(format_po(msgid=text_block.rstrip('\n')))
 
 
