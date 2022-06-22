099cd6d7e975a19110ff9f8a1aed923f7694a081
insolor <insolor@gmail.com>
2022-06-22 14:56:29 +0300

    Test exception on wrong mo file signature

--- tests/test_parse_mo.py
+++ tests/test_parse_mo.py
@@ -1,7 +1,8 @@
-from io import BytesIO
-from typing import Mapping, List, Tuple
+import io
 
-from df_gettext_toolkit.parse_mo import create_mo, load_mo, MO_MAGIC
+import pytest
+
+from df_gettext_toolkit.parse_mo import create_mo, load_mo
 
 
 def test_load_mo():
@@ -12,3 +13,9 @@ def test_load_mo():
     }
     mo_file = create_mo(d)
     assert {item['msgid']: item['msgstr'] for item in load_mo(mo_file)} == d
+
+
+def test_wrong_mo_signature():
+    file = io.BytesIO(b"foobar")
+    with pytest.raises(ValueError):
+        next(load_mo(file))
