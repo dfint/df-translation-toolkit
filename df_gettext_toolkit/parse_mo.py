3577ecece135a0a7d93f7b319711f995f663b20b
insolor <insolor@gmail.com>
2022-06-24 10:47:26 +0300

    Use TranslationItem in parse_mo, add support of contexts in create_mo, improve tests

--- df_gettext_toolkit/parse_mo.py
+++ df_gettext_toolkit/parse_mo.py
@@ -1,9 +1,7 @@
 from io import BytesIO
-from typing import List, Mapping, Tuple
+from typing import List, Tuple, Iterator, Sequence
 
-import typer
-
-from .parse_po import format_po_item
+from df_gettext_toolkit import TranslationItem
 
 MO_MAGIC = b"\xde\x12\x04\x95"
 
@@ -20,7 +18,7 @@ def load_string(file_object, offset) -> bytes:
     return file_object.read(string_size)
 
 
-def load_mo(mo_file, encoding="utf-8"):
+def load_mo(mo_file, encoding="utf-8") -> Iterator[TranslationItem]:
     mo_file.seek(0)
     magic_number = mo_file.read(4)
     if magic_number != MO_MAGIC:
@@ -31,27 +29,22 @@ def load_mo(mo_file, encoding="utf-8"):
     original_string_table_offset = read_uint(mo_file)
     translation_string_table_offset = read_uint(mo_file)
     for i in range(number_of_strings):
-        original_string = load_string(mo_file, original_string_table_offset + i * 8).decode(encoding)
+        context, _, original_string = load_string(mo_file, original_string_table_offset + i * 8).rpartition(b"\x04")
+        original_string = original_string.decode(encoding)
         translation_string = load_string(mo_file, translation_string_table_offset + i * 8).decode(encoding)
 
-        context, _, original_string = original_string.rpartition("\x04")
         if context:
-            yield dict(msgctxt=context, msgid=original_string, msgstr=translation_string)
+            context = context.decode(encoding)
+            yield TranslationItem(context=context, text=original_string, translation=translation_string)
         else:
-            yield dict(msgid=original_string, msgstr=translation_string)
-
-
-def show_mo_content(filename: str):
-    with open(filename, "rb") as mo_file:
-        for item in load_mo(mo_file):
-            print(format_po_item(**item), end="\n\n")
+            yield TranslationItem(text=original_string, translation=translation_string)
 
 
 def write_uint(file_object: BytesIO, d: int):
     file_object.write(d.to_bytes(4, "little"))
 
 
-def create_mo(dictionary: Mapping[str, str]) -> BytesIO:
+def create_mo(dictionary: Sequence[TranslationItem], encoding="utf-8") -> BytesIO:
     mo_file = BytesIO()
     mo_file.write(MO_MAGIC)
 
@@ -62,11 +55,18 @@ def create_mo(dictionary: Mapping[str, str]) -> BytesIO:
     originals_string_table: List[Tuple[int, int]] = []
     translations_string_table: List[Tuple[int, int]] = []
 
-    for string, translation in dictionary.items():
+    for item in dictionary:
+        if item.context is None:
+            string = item.text.encode(encoding)
+        else:
+            string = item.context.encode(encoding) + b"\x04" + item.text.encode(encoding)
+
+        translation = item.translation.encode(encoding)
+
         originals_string_table.append((len(strings_block.getbuffer()), len(string)))
-        strings_block.write(string.encode("utf-8"))
+        strings_block.write(string)
         translations_string_table.append((len(strings_block.getbuffer()), len(translation)))
-        strings_block.write(translation.encode("utf-8"))
+        strings_block.write(translation)
 
     original_string_table_offset = len(mo_file.getbuffer()) + 8
     translations_string_table_offset = original_string_table_offset + 8 * len(originals_string_table)
@@ -86,7 +86,3 @@ def create_mo(dictionary: Mapping[str, str]) -> BytesIO:
     mo_file.write(strings_block.getbuffer())
 
     return mo_file
-
-
-if __name__ == "__main__":
-    typer.run(show_mo_content)
--- tests/test_parse_mo.py
+++ tests/test_parse_mo.py
@@ -2,17 +2,18 @@ import io
 
 import pytest
 
+from df_gettext_toolkit import TranslationItem
 from df_gettext_toolkit.parse_mo import create_mo, load_mo
 
 
 def test_load_mo():
-    d = {
-        "Word1": "Translation1",
-        "Word2": "Translation2",
-        "LongWord": "LongTranslation",
-    }
+    d = [
+        TranslationItem("Word1", "Translation1"),
+        TranslationItem("Word2", "Translation2"),
+        TranslationItem("LongWord", "LongTranslation", context="context"),
+    ]
     mo_file = create_mo(d)
-    assert {item["msgid"]: item["msgstr"] for item in load_mo(mo_file)} == d
+    assert list(load_mo(mo_file)) == d
 
 
 def test_wrong_mo_signature():
