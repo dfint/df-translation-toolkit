06853844dbcb13003b031cb1671a0f1dac6d6b55
insolor <insolor@gmail.com>
2022-06-24 11:08:12 +0300

    Refactoring

--- df_gettext_toolkit/parse_mo.py
+++ df_gettext_toolkit/parse_mo.py
@@ -1,5 +1,5 @@
 from io import BytesIO
-from typing import List, Tuple, Iterator, Sequence
+from typing import List, Iterator, Sequence, NamedTuple
 
 from df_gettext_toolkit import TranslationItem
 
@@ -30,9 +30,10 @@ def load_mo(mo_file, encoding="utf-8") -> Iterator[TranslationItem]:
     translation_string_table_offset = read_uint(mo_file)
     for i in range(number_of_strings):
         context, _, original_string = load_string(mo_file, original_string_table_offset + i * 8).rpartition(b"\x04")
-        original_string = original_string.decode(encoding)
-        translation_string = load_string(mo_file, translation_string_table_offset + i * 8).decode(encoding)
+        translation_string = load_string(mo_file, translation_string_table_offset + i * 8)
 
+        original_string = original_string.decode(encoding)
+        translation_string = translation_string.decode(encoding)
         if context:
             context = context.decode(encoding)
             yield TranslationItem(context=context, text=original_string, translation=translation_string)
@@ -45,15 +46,19 @@ def write_uint(file_object: BytesIO, d: int):
 
 
 def create_mo(dictionary: Sequence[TranslationItem], encoding="utf-8") -> BytesIO:
-    mo_file = BytesIO()
-    mo_file.write(MO_MAGIC)
+    class OffsetSizePair(NamedTuple):
+        offset: int
+        size: int
 
-    mo_file.seek(8)
-    mo_file.write(len(dictionary).to_bytes(4, "little"))
+    mo_file_buffer = BytesIO()
+    mo_file_buffer.write(MO_MAGIC)
+
+    mo_file_buffer.seek(8)
+    mo_file_buffer.write(len(dictionary).to_bytes(4, "little"))
 
     strings_block = BytesIO()
-    originals_string_table: List[Tuple[int, int]] = []
-    translations_string_table: List[Tuple[int, int]] = []
+    originals_string_table: List[OffsetSizePair] = []
+    translations_string_table: List[OffsetSizePair] = []
 
     for item in dictionary:
         if item.context is None:
@@ -63,26 +68,28 @@ def create_mo(dictionary: Sequence[TranslationItem], encoding="utf-8") -> BytesI
 
         translation = item.translation.encode(encoding)
 
-        originals_string_table.append((len(strings_block.getbuffer()), len(string)))
+        originals_string_table.append(OffsetSizePair(len(strings_block.getbuffer()), len(string)))
         strings_block.write(string)
-        translations_string_table.append((len(strings_block.getbuffer()), len(translation)))
+
+        translations_string_table.append(OffsetSizePair(len(strings_block.getbuffer()), len(translation)))
         strings_block.write(translation)
 
-    original_string_table_offset = len(mo_file.getbuffer()) + 8
+    original_string_table_offset = len(mo_file_buffer.getbuffer()) + 8
+    write_uint(mo_file_buffer, original_string_table_offset)
+
     translations_string_table_offset = original_string_table_offset + 8 * len(originals_string_table)
-    write_uint(mo_file, original_string_table_offset)
-    write_uint(mo_file, translations_string_table_offset)
+    write_uint(mo_file_buffer, translations_string_table_offset)
 
     strings_block_start_offset = translations_string_table_offset + 8 * len(translations_string_table)
 
     for offset, size in originals_string_table:
-        write_uint(mo_file, size)
-        write_uint(mo_file, strings_block_start_offset + offset)
+        write_uint(mo_file_buffer, size)
+        write_uint(mo_file_buffer, strings_block_start_offset + offset)
 
     for offset, size in translations_string_table:
-        write_uint(mo_file, size)
-        write_uint(mo_file, strings_block_start_offset + offset)
+        write_uint(mo_file_buffer, size)
+        write_uint(mo_file_buffer, strings_block_start_offset + offset)
 
-    mo_file.write(strings_block.getbuffer())
+    mo_file_buffer.write(strings_block.getbuffer())
 
-    return mo_file
+    return mo_file_buffer
