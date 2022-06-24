632f209580137d2cdf6ede7663711d369a269c3f
insolor <insolor@gmail.com>
2022-06-24 12:18:45 +0300

    Refactor create_pot_from_speech.py

--- df_gettext_toolkit/create_pot_from_speech.py
+++ df_gettext_toolkit/create_pot_from_speech.py
@@ -1,24 +1,30 @@
 import sys
 from pathlib import Path
+from typing import Iterator
 
 import typer
 
-from .parse_po import format_po_item
+from df_gettext_toolkit.common import TranslationItem
+from df_gettext_toolkit.parse_po import save_pot
 
 
-def main(path: Path):
-    print("Path:", path, file=sys.stderr)
-
+def convert_file_lines_to_translation_items(path: Path) -> Iterator[TranslationItem]:
     for file_path in sorted(path.glob("*.txt")):
         if file_path.is_file():
             print("File:", file_path.name, file=sys.stderr)
             with open(file_path) as file:
                 for i, line in enumerate(file, 1):
-                    if line.rstrip("\n"):
-                        print(
-                            format_po_item(msgid=line.rstrip("\n"), file_name=file_path.name, line_number=i),
-                            end="\n\n",
-                        )
+                    text = line.rstrip("\n")
+                    if text:
+                        yield TranslationItem(text=text, source_file=file_path.name, line_number=i)
+
+
+def main(
+    path: Path,
+    destination_file: typer.FileTextWrite = typer.Option(..., encoding="utf-8"),
+):
+    print("Path:", path, file=sys.stderr)
+    save_pot(destination_file, convert_file_lines_to_translation_items(path))
 
 
 if __name__ == "__main__":
