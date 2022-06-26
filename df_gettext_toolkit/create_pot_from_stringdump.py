38dc00cd958aff91bc9fa6bc74b4a64a1e32be2c
insolor <insolor@gmail.com>
2022-06-26 16:22:05 +0300

    Fix create_pot_from_stringdump.py, add file names and line numbers

--- df_gettext_toolkit/create_pot_from_stringdump.py
+++ df_gettext_toolkit/create_pot_from_stringdump.py
@@ -1,4 +1,6 @@
-﻿import typer
+﻿from pathlib import Path
+
+import typer
 
 from df_gettext_toolkit.common import TranslationItem
 from df_gettext_toolkit.df_ignore_string_rules import all_ignore_rules, dont_ignore
@@ -7,12 +9,17 @@ from df_gettext_toolkit.parse_po import save_pot
 
 def main(
     source_file: typer.FileText,
-    destination_file: typer.FileTextWrite = typer.Option(..., encoding="utf-8"),
+    destination_file: typer.FileTextWrite,
     no_ignore: bool = False,
 ):
     template = (line.rstrip("\n") for line in source_file)
     ignore_rules = dont_ignore if no_ignore else all_ignore_rules
-    filtered_lines = (TranslationItem(text=line) for line in template if not ignore_rules(line))
+    file_name = Path(source_file.name).name
+    filtered_lines = (
+        TranslationItem(text=line, source_file=file_name, line_number=line_number)
+        for line_number, line in enumerate(template, 1)
+        if not ignore_rules(line)
+    )
     save_pot(destination_file, filtered_lines)
 
 
