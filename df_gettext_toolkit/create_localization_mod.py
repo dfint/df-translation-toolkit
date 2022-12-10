c1c2ac5aab541bf2853610141d8dd6389290692e
insolor <insolor@gmail.com>
2022-12-10 21:38:36 +0300

    Separate function traverse_vanilla_directory

--- df_gettext_toolkit/create_localization_mod.py
+++ df_gettext_toolkit/create_localization_mod.py
@@ -1,7 +1,18 @@
 from pathlib import Path
+from typing import Iterable
 
 import typer
 from loguru import logger
+
+
+def traverse_vanilla_directory(vanilla_path: Path) -> Iterable[Path]:
+    for directory in sorted(vanilla_path.glob("vanilla_*")):
+        if directory.is_dir():
+            objects = directory / "objects"
+            if objects.exists() and objects.is_dir():
+                for file in sorted(objects.glob("*.txt")):
+                    if file.is_file():
+                        yield file
 
 
 def parse_file(file_path: Path):
@@ -10,14 +21,8 @@ def parse_file(file_path: Path):
 
 # def main(raws_path: Path, pot_file: typer.FileTextWrite, source_encoding: str = "cp437"):
 def main(vanilla_path: Path):
-    for directory in vanilla_path.glob("vanilla_*"):
-        if directory.is_dir():
-            objects = directory / "objects"
-            if objects.exists():
-                for file in objects.glob("*.txt"):
-                    if file.is_file():
-                        logger.info(file)
-                        parse_file(file)
+    for file in traverse_vanilla_directory(vanilla_path):
+        logger.info(file.relative_to(vanilla_path))
 
 
 if __name__ == "__main__":
