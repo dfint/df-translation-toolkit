d437a510fb7c815ca02d6942a48c9f0a4eef30f8
insolor <insolor@gmail.com>
2022-12-10 22:51:22 +0300

    draft of get_raw_object_type function

--- df_gettext_toolkit/create_localization_mod.py
+++ df_gettext_toolkit/create_localization_mod.py
@@ -4,6 +4,8 @@ from typing import Iterable
 import typer
 from loguru import logger
 
+from df_gettext_toolkit.parse_raws import split_tag
+
 
 def traverse_vanilla_directory(vanilla_path: Path) -> Iterable[Path]:
     for directory in sorted(vanilla_path.glob("vanilla_*")):
@@ -13,6 +15,14 @@ def traverse_vanilla_directory(vanilla_path: Path) -> Iterable[Path]:
                 for file in sorted(objects.glob("*.txt")):
                     if file.is_file():
                         yield file
+
+
+def get_raw_object_type(file_name: Path) -> str:
+    with open(file_name) as file:
+        next(file)  # file name
+        next(file)  # empty line
+        object_tag = split_tag(next(file))
+        return object_tag[1]
 
 
 def parse_file(file_path: Path):
