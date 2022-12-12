efc927fd99d5daf488dad50d90f69a3e5cb2f4aa
insolor <insolor@gmail.com>
2022-12-12 21:29:14 +0300

    Get raw file type

--- df_gettext_toolkit/create_localization_mod.py
+++ df_gettext_toolkit/create_localization_mod.py
@@ -4,7 +4,7 @@ from typing import Iterable
 import typer
 from loguru import logger
 
-from df_gettext_toolkit.parse_raws import split_tag
+from df_gettext_toolkit.parse_raws import split_tag, tokenize_raw_file
 
 
 def traverse_vanilla_directory(vanilla_path: Path) -> Iterable[Path]:
@@ -17,22 +17,24 @@ def traverse_vanilla_directory(vanilla_path: Path) -> Iterable[Path]:
                         yield file
 
 
-def get_raw_object_type(file_name: Path) -> str:
-    with open(file_name) as file:
-        next(file)  # file name
-        next(file)  # empty line
-        object_tag = split_tag(next(file))
-        return object_tag[1]
+def get_raw_object_type(file_name: Path, source_encoding: str) -> str:
+    with open(file_name, encoding=source_encoding) as file:
+        for item in tokenize_raw_file(file):
+            if item.is_tag:
+                object_tag = split_tag(item.text)
+                assert object_tag[0] == "OBJECT"
+                return object_tag[1]
 
 
-def parse_file(file_path: Path):
-    ...
+def parse_file(file_path: Path, source_encoding: str):
+    logger.info(get_raw_object_type(file_path, source_encoding))
 
 
 # def main(raws_path: Path, pot_file: typer.FileTextWrite, source_encoding: str = "cp437"):
-def main(vanilla_path: Path):
+def main(vanilla_path: Path, source_encoding: str = "cp437"):
     for file in traverse_vanilla_directory(vanilla_path):
         logger.info(file.relative_to(vanilla_path))
+        parse_file(file, source_encoding)
 
 
 if __name__ == "__main__":
