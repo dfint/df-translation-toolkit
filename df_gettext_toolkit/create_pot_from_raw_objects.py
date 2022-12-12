9c282afdcf468ca9e7dbcc8b0bd2e09921b0703d
insolor <insolor@gmail.com>
2022-12-12 23:22:10 +0300

    Extraction from steam text files

--- df_gettext_toolkit/create_pot_from_raw_objects.py
+++ df_gettext_toolkit/create_pot_from_raw_objects.py
@@ -8,16 +8,20 @@ from df_gettext_toolkit.parse_po import save_pot
 from df_gettext_toolkit.parse_raws import extract_translatables_from_raws
 
 
+def extract_from_raw_file(file_name: Path, source_encoding: str) -> Iterator[TranslationItem]:
+    with open(file_name, encoding=source_encoding) as file:
+        for item in extract_translatables_from_raws(file):
+            item.source_file = file_name.name
+            yield item
+
+
 def extract_translatables_from_raws_batch(raw_files: Iterator[Path], source_encoding: str) -> Iterator[TranslationItem]:
     """
     Read all translatable items from all raw files
     """
     for file_name in raw_files:
         if file_name.is_file():
-            with open(file_name, encoding=source_encoding) as file:
-                for item in extract_translatables_from_raws(file):
-                    item.source_file = file_name.name
-                    yield item
+            yield from extract_from_raw_file(file_name, source_encoding)
 
 
 def create_pot_file(pot_file: TextIO, raw_files: Iterator[Path], source_encoding: str):
--- df_gettext_toolkit/create_pot_from_speech.py
+++ df_gettext_toolkit/create_pot_from_speech.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-from typing import Iterable, Iterator, Sequence
+from typing import Iterable, Iterator, Sequence, TextIO
 
 import typer
 
@@ -8,15 +8,19 @@ from df_gettext_toolkit.common import TranslationItem
 from df_gettext_toolkit.parse_po import save_pot
 
 
+def extract_from_speech_file(file: TextIO, source_path: str):
+    for i, line in enumerate(file, 1):
+        text = line.rstrip("\n")
+        if text:
+            yield TranslationItem(text=text, source_file=source_path, line_number=i)
+
+
 def extract_translatables(files: Iterable[Path]) -> Iterator[TranslationItem]:
     for file_path in files:
         if file_path.is_file():
             print("File:", file_path.name, file=sys.stderr)
             with open(file_path) as file:
-                for i, line in enumerate(file, 1):
-                    text = line.rstrip("\n")
-                    if text:
-                        yield TranslationItem(text=text, source_file=file_path.name, line_number=i)
+                yield from extract_from_speech_file(file, file_path.name)
 
 
 def create_pot_file(pot_file: typer.FileTextWrite, files: Sequence[Path]):
