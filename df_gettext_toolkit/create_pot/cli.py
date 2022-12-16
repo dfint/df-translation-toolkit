ff34bc17ee5fb21e790fa61a2af64a9252f7dfad
insolor <insolor@gmail.com>
2022-12-16 18:32:52 +0300

    isort

--- df_gettext_toolkit/create_pot/cli.py
+++ df_gettext_toolkit/create_pot/cli.py
@@ -1,6 +1,6 @@
 import typer
 
-from df_gettext_toolkit.create_pot import from_string_dump, from_steam_text
+from df_gettext_toolkit.create_pot import from_steam_text, from_string_dump
 
 app = typer.Typer()
 app.command(name="from_string_dump")(from_string_dump.main)
