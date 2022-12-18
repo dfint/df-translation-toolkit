a5a3ab0eaf4b24fa3e55ce0575f552bc31d4bd0c
insolor <insolor@gmail.com>
2022-12-18 22:52:46 +0300

    Some developments on csv_bisect

--- df_gettext_toolkit/csv_bisect/csv_bisect.py
+++ df_gettext_toolkit/csv_bisect/csv_bisect.py
@@ -1,3 +1,33 @@
+import shutil
+from pathlib import Path
 
-def main():
-    ...
+from df_gettext_toolkit.utils import csv_utils
+from df_gettext_toolkit.utils.backup import backup
+
+
+def split_left(start, end):
+    mid = (start + end) // 2
+    return start, mid
+
+
+def split_right(start, end):
+    mid = (start + end) // 2
+    return mid, end
+
+
+def main(csv_file: Path, encoding: str):
+    assert csv_file.is_file(), f"{csv_file.name} is not a file"
+
+    with backup(csv_file) as backup_path:
+        with open(backup_path, encoding=encoding,  newline="") as file:
+            csv_reader = csv_utils.reader(file)
+            data = list(csv_reader)
+
+        prev = (0, len(data))
+        stack = [prev]
+
+        while prev[0] < prev[1]:
+            break
+
+    # Restore backup
+    shutil.copy(csv_file, backup_path)
