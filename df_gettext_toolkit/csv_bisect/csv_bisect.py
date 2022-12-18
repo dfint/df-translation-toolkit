e2b7c8b79746172de4c677c037253ef4a9323ebc
insolor <insolor@gmail.com>
2022-12-18 23:23:04 +0300

    Add bisect function (not finished yet)

--- df_gettext_toolkit/csv_bisect/csv_bisect.py
+++ df_gettext_toolkit/csv_bisect/csv_bisect.py
@@ -1,5 +1,6 @@
 import shutil
 from pathlib import Path
+from typing import List, Tuple
 
 from df_gettext_toolkit.utils import csv_utils
 from df_gettext_toolkit.utils.backup import backup
@@ -13,6 +14,36 @@ def split_left(start, end):
 def split_right(start, end):
     mid = (start + end) // 2
     return mid, end
+
+
+def bisect(data: List[Tuple[str, str]], start: int, end: int) -> bool:
+    """
+    returns:
+    - True -> found
+    - False -> not found
+    """
+    if start == end - 1:
+        print(f"Found string index {start}:")
+        print(data[start])
+        return True
+    elif start >= end:
+        print("Empty slice, step back")
+        return False
+    else:
+        print(f"From {start} to {end} (in total {end - start})")
+        # TODO: write csv
+
+        answer = input("[G]ood/[B]ad/Step [U]p").upper()
+        if answer == "G":
+            return False
+        elif answer == "B":
+            print("Trying left half")
+            result = bisect(data, *split_left(start, end))
+            if result:
+                return result
+
+            print("Trying right half")
+            return bisect(data, *split_right(start, end))
 
 
 def main(csv_file: Path, encoding: str):
