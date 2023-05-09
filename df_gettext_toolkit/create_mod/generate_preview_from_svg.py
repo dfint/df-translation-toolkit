b0ec88ca51c830a0b0bc8e9dc7bf60f701f410c6
insolor <insolor@gmail.com>
2023-05-10 00:40:54 +0300

    Generate preview png from svg

---
+++ df_gettext_toolkit/create_mod/generate_preview_from_svg.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+import jinja2
+import typer
+from cairosvg import svg2png
+
+
+def generate_preview(template_text: str, title: str, description: str, destination_path: Path):
+    template = jinja2.Template(template_text)
+    result_svg = template.render(title=title, description=description)
+    svg2png(bytestring=result_svg, write_to=str(destination_path))
+
+
+def main(template_path: Path, title: str, description: str, destination_path: Path):
+    with open(template_path) as template_file:
+        template = template_file.read()
+        generate_preview(template, title, description, destination_path)
+
+
+if __name__ == "__main__":
+    typer.run(main)
