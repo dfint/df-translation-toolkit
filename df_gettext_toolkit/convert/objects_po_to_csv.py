c76262257fbd248ee0ec9d06c8dd40f2b704386e
insolor <insolor@gmail.com>
2023-08-02 21:29:49 +0300

    A command key to append csv

--- df_gettext_toolkit/convert/objects_po_to_csv.py
+++ df_gettext_toolkit/convert/objects_po_to_csv.py
@@ -11,13 +11,14 @@ app = typer.Typer()
 
 
 @app.command()
-def main(po_file: Path, csv_file: Path, encoding: str):
+def main(po_file: Path, csv_file: Path, encoding: str, append: bool = False):
     """
     Convert a po file into a csv file in a specified encoding
     """
 
     with open(po_file, "r", encoding="utf-8") as pofile:
-        with open(csv_file, "w", newline="", encoding=encoding, errors="replace") as outfile:
+        mode = "a" if append else "w"
+        with open(csv_file, mode, newline="", encoding=encoding, errors="replace") as outfile:
             convert(pofile, outfile, encoding)
 
 
