54906b5c63b1af70a254ef76df73a606ad854f0c
insolor <insolor@gmail.com>
2023-10-02 14:06:05 +0300

    Improve a test for ValidationException str

--- tests/test_validation_models.py
+++ tests/test_validation_models.py
@@ -8,7 +8,7 @@ from df_translation_toolkit.validation.validation_models import ProblemSeverity,
     [
         (ProblemSeverity.ERROR, "Error"),
         (ProblemSeverity.WARNING, "Warning"),
-    ]
+    ],
 )
 def test_problem_severity_str(test_data, expected):
     assert str(test_data) == expected
@@ -19,7 +19,7 @@ def test_problem_severity_str(test_data, expected):
     [
         (ValidationProblem("Error message"), "Error: Error message"),
         (ValidationProblem("Warning message", ProblemSeverity.WARNING), "Warning: Warning message"),
-    ]
+    ],
 )
 def test_validation_problem_str(test_data, expected):
     assert str(test_data) == expected
@@ -30,7 +30,7 @@ def test_validation_problem_str(test_data, expected):
     [
         ([ValidationProblem("Error")], True),
         ([ValidationProblem("Warning", ProblemSeverity.WARNING)], False),
-    ]
+    ],
 )
 def test_validation_problem_contains_errors(test_data, expected):
     assert ValidationProblem.contains_errors(test_data) == expected
@@ -39,9 +39,12 @@ def test_validation_problem_contains_errors(test_data, expected):
 @pytest.mark.parametrize(
     "test_data, expected",
     [
-        ([ValidationProblem("Error message")], "Error: Error message"),
+        (
+            [ValidationProblem("Error message"), ValidationProblem("Warning message", ProblemSeverity.WARNING)],
+            "Error: Error message\nWarning: Warning message",
+        ),
         ([ValidationProblem("Warning message", ProblemSeverity.WARNING)], "Warning: Warning message"),
-    ]
+    ],
 )
 def test_validation_exception_str(test_data, expected):
     assert str(ValidationException(test_data)) == expected
