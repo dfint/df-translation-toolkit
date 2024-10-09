7c0f929866c08b903679b2a7d14ad2b23cdca9fe
insolor <2442833+insolor@users.noreply.github.com>
2024-10-09 12:43:38 +0300

    Ignore strings which are started from an underscode-delimited word

--- tests/test_df_ignore_string_rules.py
+++ tests/test_df_ignore_string_rules.py
@@ -63,6 +63,7 @@ import df_translation_toolkit.utils.df_ignore_string_rules as rules
         (rules.ignore_underline_separated_words, "index1_11", True),
         (rules.ignore_underline_separated_words, "_Thrd_id", True),
         (rules.ignore_underline_separated_words, "_initterm_e", True),
+        (rules.ignore_underline_separated_words, "init_sound returned false!", True),
         (rules.ignore_dash_prepended_strings, "-world_sites_and_pops", True),
         (rules.ignore_dash_prepended_strings, "-site_map-", True),
         (
