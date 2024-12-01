4b4d64ffc3db606c4926bfd5c88baf4b6898a212
insolor <2442833+insolor@users.noreply.github.com>
2024-12-01 23:03:43 +0300

    Add a rule for dash-separated words (#116)

--- df_translation_toolkit/utils/df_ignore_string_rules.py
+++ df_translation_toolkit/utils/df_ignore_string_rules.py
@@ -103,6 +103,26 @@ def ignore_dash_prepended_strings(string: str) -> bool:
 
 
 @rules.register
+def ignore_dash_separated_words(string: str) -> bool:
+    if " " in string:
+        return False
+
+    if "-" not in string:
+        return False
+
+    parts = string.split("-")
+    for part in parts:
+        if not part or not (part.islower() or part.isnumeric()):
+            return False
+
+    if len(parts) >= 3:  # noqa: PLR2004
+        return True
+
+    ending = parts[-1]
+    return ending.isnumeric() or ending in ("on", "off", "log", "gtr", "rtm")
+
+
+@rules.register
 def ignore_mixed_case(string: str) -> bool:
     return re.search(r"[a-z]+[A-Z]", string) is not None
 
