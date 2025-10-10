40afca08085e64c2d2da9b57ee002bbb3a4df310
insolor <2442833+insolor@users.noreply.github.com>
2025-10-10 17:19:29 +0300

    Pass Diagnostics object instead of file object  buffer to get validation problems (#155)

--- df_translation_toolkit/validation/validation_models.py
+++ df_translation_toolkit/validation/validation_models.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from dataclasses import dataclass, field
 from enum import Enum, auto
 from typing import NamedTuple
 
@@ -30,3 +31,33 @@ class ValidationException(Exception):
 
     def __str__(self) -> str:
         return "\n".join(str(error) for error in self.problems)
+
+
+@dataclass
+class ProblemInfo:
+    original: str
+    translation: str
+    problems: list[ValidationProblem] = field(default_factory=list)
+
+    def __str__(self) -> str:
+        problems_text = "\n".join(str(problem) for problem in self.problems)
+        return f"Problematic tag pair: {self.original!r}, {self.translation!r}\nProblems:\n{problems_text}"
+
+    def contains_errors(self) -> bool:
+        return ValidationProblem.contains_errors(self.problems)
+
+
+class Diagnostics:
+    problems: list[ProblemInfo]
+
+    def __init__(self) -> None:
+        self.problems = []
+
+    def add(self, problem_info: ProblemInfo) -> None:
+        self.problems.append(problem_info)
+
+    def __bool__(self) -> bool:
+        return bool(self.problems)
+
+    def contains_errors(self) -> bool:
+        return any(problem.contains_errors() for problem in self.problems)
