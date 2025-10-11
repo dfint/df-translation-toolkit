49e02f39ceaed1a511dc244082ec6e9031dd1d86
insolor <2442833+insolor@users.noreply.github.com>
2025-10-11 16:08:52 +0300

    Add `__str__` method to Diagnostics

--- df_translation_toolkit/validation/validation_models.py
+++ df_translation_toolkit/validation/validation_models.py
@@ -61,3 +61,6 @@ class Diagnostics:
 
     def contains_errors(self) -> bool:
         return any(problem.contains_errors() for problem in self.problems)
+
+    def __str__(self) -> str:
+        return "\n".join(map(str, self.problems))
